bc985e1ee163a62c11ed1e6dac2f247ca5cae7dd
CaseyGann <cmoden@gannacademy.org>
2023-09-11 10:30:05 -0400

    init

---
+++ src/App.java
@@ -0,0 +1,25 @@
+/** Task:
+ *  1. Write code that will take in two separate values from the user.
+ *  2. Write code that will accurately produce one of the following statements based on the given inputs from Step #1:
+ *       "Both numbers were divisible by 3!"
+ *       "Only one of the numbers was divisble by 3"
+ *       "Neither number was divisible by 3"
+ *  3. Your code should work for any input value.
+ * 
+ *  For example:
+ *    With inputs of 6 and 3, the output should be: "Both numbers were divisible by 3!"
+ *    With inputs of 6 and 4, the output should be: "Only one of the numbers was divisble by 3"
+ *    With inputs of 4 and 2, the output should be: "Neither number was divisible by 3"
+ * 
+ */
+
+import java.util.*;
+
+public class App {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+    }
+    
+}
